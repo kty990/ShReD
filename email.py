8af54357f86d7f2721e159599b47cfbfa0004834
kty990 <41134070+kty990@users.noreply.github.com>
2018-07-12 21:12:26 -0600

    Update email.py

--- email.py
+++ email.py
@@ -1,5 +1,33 @@
 import smtplib
 
+print("Contacts do not reset when file is closed...")
+
+while contact_name != "" and contact_email != "":
+    contact_name = input("Enter contact name here -> ")
+    contact_email = input("Enter contact email here -> ")
+    if contact_name != "" and contact_email != "":
+        getContacts(contact_name, contact_email)
+    else:
+        print("Ok, I see you have no more contacts to add, lets continue with the scheduled program")
+
+contacts = open("contacts.txt", "w")
+name = open("name.txt", "w")
+
+def getContacts(name, email):
+    contacts.write("\n" + email)
+    contacts.close()
+    name.write("\n" + name)
+    name.close()
+
+def useContacts(name):
+    if name.readline(i) == name:
+        target = email.readline(i)
+    elif name.readline(i) == "":
+        print("Your contact does not exist!")
+        break
+    else:
+        i += 1
+
 content = 'content'
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -10,37 +38,51 @@ server.starttls()
 """ Get the login information of the user """
 user = input('Enter your gmail username (without @gmail.com) -> ')
 pw = input('Enter the password that goes with this account -> ')
+username = user + "@gmail.com"
 
 """ Get the information attatched to the email """
 text = input('What do you want the body of the email to say? -> ')
 subject = input('What is the subject of the email? -> ')
-msg = "Subject {}\n\n{}".format(subject, msg)
-target = input('Who do you want to send the email to? -> ')
+msg = "Subject {}\n\n{}".format(subject, text)
+contact_use = input("Do you want to use contacts? [1] = yes [2] = no -> ")
+if contact_use == "1":
+    print("(It is case-sensitive!)")
+    chosen_name = input("What is the name of your contact?")
+    useContacts(chosen_name)
+if contact_use != "1":
+    target = input('Who do you want to send the email to? -> ')
 
 server.login(user, pw)
-print('Login for %s successful'%user)
+print('Login for %s successful'%username)
 
 print('Confirm email send? [Y] or [N]')
 ans = input('-> ')
 if ans == "Y":
-    server.send(user, target, msg)
+    server.send(username, target, msg)
 
 while True:
     checkInfo = input("Do you want to change the info? [Y] or [N] -> ")
     if checkInfo == "Y":
         user = input('Enter your gmail username (without @gmail.com) -> ')
         pw = input('Enter the password that goes with this account -> ')
+        username = user + "@gmail.com"
         text = input('What do you want the body of the email to say? -> ')
         subject = input('What is the subject of the email? -> ')
         msg = "Subject {}\n\n{}".format(subject, msg)
-        target = input('Who do you want to send the email to? -> ')
+        contact_use = input("Do you want to use contacts? [1] = yes [2] = no -> ")
+        if contact_use == "1":
+            print("(It is case-sensitive!)")
+            chosen_name = input("What is the name of your contact?")
+            useContacts(chosen_name)
+        if contact_use != "1":
+            target = input('Who do you want to send the email to? -> ')
         server.ehlo()
         server.starttls()
-        server.login(user, pw)
-        print('Login for %s successful'%user)
+        server.login(username, pw)
+        print('Login for %s successful'%username)
         command = input("What shall it be boss? -> ")
         if command == "single":
-            server.send(user, target, msg)
+            server.send(username, target, msg)
         if command == "spam":
             num = input("How many emails? (* means infinite) -> ")
             if num != "*":
@@ -49,23 +91,24 @@ while True:
             if num == "*":
                 while True:
                     server.send(user, target, msg)
+    """ It just continues along if the answer is not Y """
     command = input("What shall it be boss? -> ")
     if command == "single":
         server.ehlo()
         server.starttls()
-        server.login(user, pw)
-        server.send(user, target, msg)
+        server.login(username, pw)
+        server.send(username, target, msg)
     if command == "spam":
         num = input("How many emails? (* means infinite) -> ")
         if num != "*":
             for x in range(0, int(num)):
                 server.ehlo()
                 server.starttls()
-                server.login(user, pw)
-                server.send(user, target, msg)
+                server.login(username, pw)
+                server.send(username, target, msg)
         if num == "*":
-            server.ehlo()
-            server.starttls()
-            server.login(user, pw)
-            while True:
-                server.send(user, target, msg)
+            while True:    
+                server.ehlo()
+                server.starttls()
+                server.login(username, pw)
+                server.send(username, target, msg)
